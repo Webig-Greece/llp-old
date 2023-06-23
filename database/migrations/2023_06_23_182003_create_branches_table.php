6e8b46ce838104e7aa27f45e557647752c546395
Panos Thodos <panosthodos@gmail.com>
2023-06-23 21:23:37 +0300

    added branches migration

--- database/migrations/2023_06_23_182003_create_branches_table.php
+++ database/migrations/2023_06_23_182003_create_branches_table.php
@@ -4,24 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBranchesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('branches');
     }
-};
+}
