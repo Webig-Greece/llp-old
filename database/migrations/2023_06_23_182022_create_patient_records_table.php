42ccf0ccd81c012b4530c25dc7e7224eaf92241b
Panos Thodos <panosthodos@gmail.com>
2023-06-23 21:44:25 +0300

    add patient records migration

--- database/migrations/2023_06_23_182022_create_patient_records_table.php
+++ database/migrations/2023_06_23_182022_create_patient_records_table.php
@@ -4,24 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePatientRecordsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('patient_records', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('branch_id');
+            $table->unsignedBigInteger('practitioner_id');
+            $table->string('patient_name');
+            $table->text('notes')->nullable();
+            $table->date('appointment_date')->nullable();
             $table->timestamps();
+
+            // Foreign key constraints
+            $table->foreign('branch_id')->references('id')->on('branches')->onDelete('cascade');
+            $table->foreign('practitioner_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('patient_records');
     }
-};
+}
