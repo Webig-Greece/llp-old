463377a73c3d0553a01c2c41bdd57bd338d32f79
Panos Thodos <panosthodos@gmail.com>
2023-06-23 22:08:47 +0300

    add Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    /**
+     * The users that belong to the role.
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * The permissions that belong to the role.
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
 }
