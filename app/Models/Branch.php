b8c62cc52c2e217475b6bc859b3768dc42504a2f
Panos Thodos <panosthodos@gmail.com>
2023-06-23 22:00:49 +0300

    add branch model

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Branch extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'name',
+        'address',
+        'phone'
+    ];
+
+    /**
+     * Get the company that owns the branch.
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
