cfddd5c8d4ad43eff9c0b0fddb3ed4b6feeb33ca
Panos Thodos <panosthodos@gmail.com>
2023-07-11 22:22:20 +0300

    add subscriptionplan controller and user controller functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,50 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\SubscriptionPlan;
+use App\Models\User;
 
-class SubscriptionPlanController extends Controller
+class UserController extends Controller
 {
     /**
-     * Display a listing of the subscription plans.
+     * Display a listing of the users.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $plans = SubscriptionPlan::all();
+        $users = User::all();
 
-        return response()->json($plans);
+        return response()->json($users);
     }
 
     /**
-     * Store a newly created subscription plan in storage.
+     * Store a newly created user in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $plan = SubscriptionPlan::create($request->all());
+        $user = User::create($request->all());
 
-        return response()->json($plan, 201);
+        return response()->json($user, 201);
     }
 
     /**
-     * Display the specified subscription plan.
+     * Display the specified user.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        $plan = SubscriptionPlan::findOrFail($id);
+        $user = User::findOrFail($id);
 
-        return response()->json($plan);
+        return response()->json($user);
     }
 
     /**
-     * Update the specified subscription plan in storage.
+     * Update the specified user in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -54,21 +54,21 @@ class SubscriptionPlanController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $plan = SubscriptionPlan::findOrFail($id);
-        $plan->update($request->all());
+        $user = User::findOrFail($id);
+        $user->update($request->all());
 
-        return response()->json($plan, 200);
+        return response()->json($user, 200);
     }
 
     /**
-     * Remove the specified subscription plan from storage.
+     * Remove the specified user from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        SubscriptionPlan::destroy($id);
+        User::destroy($id);
 
         return response()->json(null, 204);
     }
