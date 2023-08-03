249a50a013f52ed1783eb460b66cc8c8621c1652
Panos Thodos <panosthodos@gmail.com>
2023-08-04 01:55:00 +0300

    one more small fix

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -19,6 +19,7 @@ class RolesTableSeeder extends Seeder
             ['name' => 'psychologist', 'description' => 'Psychologist'],
             ['name' => 'counselor', 'description' => 'Mental Health Counselor'],
             ['name' => 'coach', 'description' => 'Life Coach'],
+            ['name' => 'psychiatrist', 'description' => 'Psychiatrist'],
             ['name' => 'secretary', 'description' => 'Secretary'],
             ['name' => 'trial_user', 'description' => 'Trial User'],
         ];
